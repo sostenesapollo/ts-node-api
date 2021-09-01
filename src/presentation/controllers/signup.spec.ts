53b7500ed1d62b292c5d2ce0ba5388e03192010b
Sostenes <sostenesapollo25@gmail.com>
2021-09-01 16:08:44 -0300

    fix: fix texts

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -30,7 +30,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
 
-  test('hould return 400 if no password is provided', () => {
+  test('Should return 400 if no password is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
@@ -44,7 +44,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
 
-  test('hould return 400 if no password confirmation is provided', () => {
+  test('Should return 400 if no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
