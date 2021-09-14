8dd0e95f1cfafcc06203ddcc69b59445d1be25b9
Sostenes <sostenesapollo25@gmail.com>
2021-09-14 16:08:22 -0300

    feat: connect to mongodb before app listen

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,7 +4,8 @@ export const MongoHelper = {
   client: MongoClient,
 
   async connect (uri: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL ?? '')
+    console.log(uri)
+    this.client = await MongoClient.connect(uri)
   },
 
   async disconnect (): Promise<void> {
