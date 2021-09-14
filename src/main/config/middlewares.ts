61007938a3e3cf323decd654644d5ff5fce68192
Sostenes <sostenesapollo25@gmail.com>
2021-09-14 10:22:17 -0300

    fix: bug codes

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,6 @@
 import { Express } from 'express'
 // import { bodyParser } from './../middlewares/body-parser'
-// import { cors } from '../middlewares/cors'
+import { cors } from '../middlewares/cors'
 // import { contentType } from '../middlewares/content-type'
 
 export default (app: Express): void => {
