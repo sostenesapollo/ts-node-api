35e3a25c0d3d97daf07522fbed06a54286d68069
Sostenes <sostenesapollo25@gmail.com>
2021-09-14 09:47:26 -0300

    feat: ensure cors is enabled

--- src/main/middlewares/body-parser.test.ts
+++ src/main/middlewares/body-parser.test.ts
@@ -1,9 +1,8 @@
 import request from 'supertest'
-import express from 'express'
 import app from '../config/app'
 
 describe('Body Parser Middleware', () => {
-  test('', async () => {
+  test('Should parse body as json', async () => {
     app.post('/test_body_parser', (req, res) => res.send(req.body))
     await request(app)
       .post('/test_body_parser')
