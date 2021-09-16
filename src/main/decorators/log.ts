4cb3d1cd73e4d9850ad585f6082d8d846f05e9d6
Sostenes <sostenesapollo25@gmail.com>
2021-09-16 10:37:50 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.ts
+++ src/main/decorators/log.ts
@@ -1,11 +1,16 @@
 import { Controller, HttpResponse } from '../../presentation/protocols'
+import { LogErrorRepository } from '../../data/protocols/log-error-repository'
 export class LogControllerDecorator implements Controller {
   constructor (
-    private readonly controller: Controller
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
   ) {}
 
   async handle (httpRequest: any): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
+    if (httpResponse.statusCode === 500) {
+      await this.logErrorRepository.log(httpResponse.body.stack)
+    }
     return httpResponse
   }
 }
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -4,7 +4,7 @@ import { SignUpController } from './../../presentation/controllers/signup/signup
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adpter'
-import { LogControllerDecorator } from '../decorators/log'
+// import { LogControllerDecorator } from '../decorators/log'
 import { Controller } from '../../presentation/protocols'
 
 export const makeSignUpController = (): Controller => {
@@ -14,5 +14,5 @@ export const makeSignUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  return signUpController
 }
