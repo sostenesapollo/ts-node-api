a6c59ce323586d609a6839d23d3d1a34f3b472bf
Sostenes <sostenesapollo25@gmail.com>
2021-09-22 18:04:19 -0300

    refactor: group validation imports in a single file

--- src/main/factories/signup/signup-validation.spec.ts
+++ src/main/factories/signup/signup-validation.spec.ts
@@ -1,10 +1,7 @@
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
-import { EmailValidator } from '../../../presentation/protocols/email-validator'
-import { CompareFieldsValidation } from '../../../presentation/helpers/validators/compare-fields-validation'
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation'
-import { Validation } from '../../../presentation/protocols/validation'
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { EmailValidation, CompareFieldsValidation, RequiredFieldValidation, ValidationComposite } from '../../../presentation/helpers/validators'
 import { makeSignUpValidation } from './signup-validation'
+import { EmailValidator } from '../../../presentation/protocols/email-validator'
+import { Validation } from '../../../presentation/protocols/validation'
 
 jest.mock('../../../presentation/helpers/validators/validation-composite')
 
--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,5 +1,4 @@
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
-
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adpter'
