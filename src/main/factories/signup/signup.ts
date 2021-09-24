89aca97ff89719145d93a439bf6446500d9f2ff4
Sostenes <sostenesapollo25@gmail.com>
2021-09-24 08:02:45 -0300

    refactor: move BracyptAdapter to his own folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adpter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adpter'
 import { LogControllerDecorator } from '../../decorators/log'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { Controller } from '../../../presentation/protocols'
