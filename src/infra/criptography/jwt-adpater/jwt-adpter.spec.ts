bf024ce9595d3061597b74fd42f361d11e10855a
Sostenes <sostenesapollo25@gmail.com>
2021-09-30 11:16:46 -0300

    test: ensure JwtAdapter returns a valid token

--- src/infra/criptography/jwt-adpater/jwt-adpter.spec.ts
+++ src/infra/criptography/jwt-adpater/jwt-adpter.spec.ts
@@ -26,6 +26,12 @@ describe('Jwt Adapter', () => {
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_value' }, 'secret')
   })
 
+  test('Should return a token on sign success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.encrypt('any_value')
+    expect(accessToken).toBe('any_token')
+  })
+
   test('Should throw if sign throws', async () => {
     const { sut } = makeSut()
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
