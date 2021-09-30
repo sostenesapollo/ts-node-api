66618f15d8bd2e5a1fe61830a9fe25d78b848de4
Sostenes <sostenesapollo25@gmail.com>
2021-09-30 17:14:35 -0300

    feat: add login route

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,13 +1,11 @@
 import env from '../../config/env'
-import { LogMongoRepository } from './../../../infra/db/mongodb/log/log-mongo-repository'
 import { LoginController } from '../../../presentation/controllers/login/login-controller'
-import { LogControllerDecorator } from '@/main/decorators/log-controller-decorator'
-import { Controller } from '@/presentation/protocols'
-import { DbAuthentication } from '@/data/usecases/authentication/db-authentication'
 import { makeLoginValidation } from './login-validation-factory'
-import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
-import { BcryptAdapter } from '@/infra/criptography/bcrypt-adapter/bcrypt-adpter'
-import { JwtAdapter } from '@/infra/criptography/jwt-adpater/jwt-adapter'
+import { AccountMongoRepository } from './../../../infra/db/mongodb/account/account-mongo-repository'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adpter'
+import { JwtAdapter } from '../../../infra/criptography/jwt-adpater/jwt-adapter'
+import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
+import { Controller } from '../../../presentation/protocols/controller'
 
 export const makeLoginController = (): Controller => {
   const salt = 12
@@ -16,6 +14,5 @@ export const makeLoginController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
   const loginController = new LoginController(dbAuthentication, makeLoginValidation())
-  const logMongoRepository = new LogMongoRepository()
-  return new LogControllerDecorator(loginController, logMongoRepository)
+  return loginController
 }
