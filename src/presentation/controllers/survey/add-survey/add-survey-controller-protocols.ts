5cd8bf7fcb7db289a6d94c55ab6aa8e61922fe0b
Sostenes <sostenesapollo25@gmail.com>
2021-10-04 10:27:30 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller-protocols.ts
@@ -1,2 +1,4 @@
-export * from './../../../protocols/http'
-export * from './../../../protocols/controller'
+export * from '../../../protocols/validation'
+export * from '../../../protocols/http'
+export * from '../../../protocols/controller'
+export * from '../../../../domain/usecases/add-survey'
