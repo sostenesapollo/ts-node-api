7ec4eea06bc2ae48981de318b95406f9a547d994
Sostenes <sostenesapollo25@gmail.com>
2021-10-06 12:17:09 -0300

    feat: ensure JwtAdapter calls verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -11,7 +11,7 @@ export class JwtAdapter implements Encrypter, Decrypter {
   }
 
   async decrypt (value: string): Promise<string> {
-    const accessToken = await jwt.sign({ id: value }, this.secret)
-    return accessToken
+    await jwt.verify(value, this.secret)
+    return null
   }
 }
