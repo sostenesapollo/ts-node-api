fa8d10af2ad025c5f40efc294528bd5a50712250
Sostenes <sostenesapollo25@gmail.com>
2021-10-06 11:01:32 -0300

    refactor: move AUthMiddleware dependencies to a protocols file

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,8 +1,6 @@
 import { LoadAccountByToken } from '../../domain/usecases/load-account-by-token'
+import { HttpRequest, HttpResponse, Middleware, forbiden, ok, serverError } from './auth-middleware-protocols'
 import { AccessDeniedError } from '../errors'
-import { forbiden, ok, serverError } from '../helpers/http/http-helper'
-import { HttpRequest, HttpResponse, Middleware } from '../protocols'
-
 export class AuthMiddleware implements Middleware {
   constructor (
     private readonly loadAccountByToken: LoadAccountByToken,
