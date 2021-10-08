8860ce4e5f71f60bd5e6fb72a3c0683c261d83f8
Sostenes <sostenesapollo25@gmail.com>
2021-10-08 08:17:01 -0300

    feat: ensure LoadSurveysControlller returns 200 on success

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -2,6 +2,7 @@ import { LoadSurveysController } from './load-surveys-controller'
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
 import { Survey } from '../../../../domain/models/survey'
 import MockDate from 'mockdate'
+import { ok } from '../../../middlewares/auth-middleware-protocols'
 
 const makeFakeSurveys = (): Survey[] => {
   return [
@@ -67,5 +68,11 @@ describe('LoadSurveys Controller', () => {
     const loadSpy = jest.spyOn(loadSurveysStub, 'load')
     await sut.handle({})
     expect(loadSpy).toHaveBeenCalled()
+  })
+
+  test('Should return 200 on success  ', async () => {
+    const { sut } = makeSut()
+    const httpReponse = await sut.handle({})
+    expect(httpReponse).toEqual(ok(makeFakeSurveys()))
   })
 })
--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.ts
@@ -1,13 +1,14 @@
 import { HttpRequest, HttpResponse } from './load-surveys-controller-protocols'
 import { Controller } from '../../../protocols/controller'
 import { LoadSurveys } from '../../../../domain/usecases/load-surveys'
+import { ok } from '../../../middlewares/auth-middleware-protocols'
 export class LoadSurveysController implements Controller {
   constructor (
     private readonly loadSurveys: LoadSurveys
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveys.load()
-    return null
+    const surveys = await this.loadSurveys.load()
+    return ok(surveys)
   }
 }
