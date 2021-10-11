05789bcd1263d11085156442357f845dee1e8d7a
Sostenes <sostenesapollo25@gmail.com>
2021-10-11 13:07:06 -0300

    feat: ensure SurveyResultMongoRespository insert new surveyResult if its new

---
+++ src/data/protocols/db/survey/load-survey-result-repository.ts
@@ -0,0 +1,6 @@
+
+import { SurveyResultModel } from '@/domain/models/survey-result'
+
+export interface LoadSurveyResultRepository {
+  loadBySurveyId: (surveyId: string, accountId: string) => Promise<SurveyResultModel>
+}
