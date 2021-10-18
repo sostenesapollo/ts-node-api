195cd2c8c5f27eced7b4f4528cbb2f8623ee9c38
Sostenes <sostenesapollo25@gmail.com>
2021-10-18 10:18:02 -0300

    refactor: remove duplicated code

--- src/data/protocols/db/survey-result/save-survey-result-respository..ts
+++
@@ -1,6 +0,0 @@
-import { SurveyResultModel } from '@/domain/models/survey-result'
-import { SaveSurveyResultParams } from '@/domain/usecases/survey-result/save-survey-result'
-
-export interface SaveSurveyResultRepository {
-  save (accountData: SaveSurveyResultParams): Promise<SurveyResultModel>
-}
